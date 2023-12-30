ba71627b2ba70753ccfefafaa0f585b3397c7748
George White <stonehippo@gmail.com>
2023-12-30 14:21:10 -0500

    enable deep sleep between calls

--- probe/code.py
+++ probe/code.py
@@ -9,6 +9,7 @@ from adafruit_si7021 import SI7021
 from adafruit_veml7700 import VEML7700
 from adafruit_lc709203f import LC709203F, PackSize
 import time
+import alarm
 import rtc
 import adafruit_ntp
 from digitalio import DigitalInOut
@@ -48,18 +49,18 @@ ntp = adafruit_ntp.NTP(pool, tz_offset=int(os.getenv("TIMEZONE_OFFSET")))
 r = rtc.RTC()
 r.datetime = ntp.datetime
 
-while True:
-	raw_temp = temp_sensor.temperature
-	c = shorten(raw_temp)
-	f = shorten(celcius_to_fahrenheit(raw_temp))
-	h = shorten(temp_sensor.relative_humidity)
-	l = shorten(light_sensor.lux)
-	b = batt.cell_percent
-	t = r.datetime
+raw_temp = temp_sensor.temperature
+c = shorten(raw_temp)
+f = shorten(celcius_to_fahrenheit(raw_temp))
+h = shorten(temp_sensor.relative_humidity)
+l = shorten(light_sensor.lux)
+b = batt.cell_percent
+t = r.datetime
 
-	print(f'{t.tm_mon}/{t.tm_mday}@{t.tm_hour}:{t.tm_min}:{t.tm_sec}: temp: {c}°C/{f}°F, humidity: {h}%, lux: {l}, batt: {b}%')
-	update_homebridge('backyard_temp', c)
-	update_homebridge('backyard_humidity', h)
-	update_homebridge('backyard_lux', l)
+print(f'{t.tm_mon}/{t.tm_mday}@{t.tm_hour}:{t.tm_min}:{t.tm_sec}: temp: {c}°C/{f}°F, humidity: {h}%, lux: {l}, batt: {b}%')
+update_homebridge('backyard_temp', c)
+update_homebridge('backyard_humidity', h)
+update_homebridge('backyard_lux', l)
 
-	time.sleep(30)
+deep_sleep = alarm.time.TimeAlarm(monotonic_time=time.monotonic() + 30)
+alarm.exit_and_deep_sleep_until_alarms(deep_sleep)
