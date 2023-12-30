819dffe2a9c95077bb8159af542034b7f3543b72
George White <stonehippo@gmail.com>
2023-12-30 14:43:37 -0500

    Remove excess code
    Took out the battery monitor code, unneeded conversion to fahrenheit,
    and debug output to the console.

--- probe/code.py
+++ probe/code.py
@@ -1,30 +1,21 @@
 import os
 import board
-import busio
 import wifi
 import socketpool
 import adafruit_requests
 import ssl
 from adafruit_si7021 import SI7021
 from adafruit_veml7700 import VEML7700
-from adafruit_lc709203f import LC709203F, PackSize
 import time
 import alarm
-import rtc
-import adafruit_ntp
 from digitalio import DigitalInOut
 
 # turn off the neopixel to save power; it's an input by default, so it's value is False
 np = DigitalInOut(board.NEOPIXEL_POWER)
 
-i2c = busio.I2C(board.SCL, board.SDA)
+i2c = board.I2C()
 temp_sensor = SI7021(i2c)
 light_sensor = VEML7700(i2c)
-batt = LC709203F(i2c)
-batt.pack_size = PackSize.MAH1000
-
-def celcius_to_fahrenheit(temperature):
-	return temperature * 1.8 + 32
 
 def shorten(value):
 	return round(value, 1)
@@ -45,19 +36,12 @@ def update_homebridge(accessory, value):
 		print("Could not connect to Homebridge")
 
 pool, requests = wifi_connect()
-ntp = adafruit_ntp.NTP(pool, tz_offset=int(os.getenv("TIMEZONE_OFFSET")))
-r = rtc.RTC()
-r.datetime = ntp.datetime
 
 raw_temp = temp_sensor.temperature
 c = shorten(raw_temp)
-f = shorten(celcius_to_fahrenheit(raw_temp))
 h = shorten(temp_sensor.relative_humidity)
 l = shorten(light_sensor.lux)
-b = batt.cell_percent
-t = r.datetime
 
-print(f'{t.tm_mon}/{t.tm_mday}@{t.tm_hour}:{t.tm_min}:{t.tm_sec}: temp: {c}°C/{f}°F, humidity: {h}%, lux: {l}, batt: {b}%')
 update_homebridge('backyard_temp', c)
 update_homebridge('backyard_humidity', h)
 update_homebridge('backyard_lux', l)
