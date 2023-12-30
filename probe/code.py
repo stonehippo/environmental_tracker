6f03e896a157d4b463a8a2cef70ec297a09fcd5b
George White <stonehippo@gmail.com>
2023-12-30 13:50:09 -0500

    Updated battery pack size

--- probe/code.py
+++ probe/code.py
@@ -20,7 +20,7 @@ i2c = busio.I2C(board.SCL, board.SDA)
 temp_sensor = SI7021(i2c)
 light_sensor = VEML7700(i2c)
 batt = LC709203F(i2c)
-batt.pack_size = PackSize.MAH400
+batt.pack_size = PackSize.MAH1000
 
 def celcius_to_fahrenheit(temperature):
 	return temperature * 1.8 + 32
